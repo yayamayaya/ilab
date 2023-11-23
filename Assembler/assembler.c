f15ba7234958126564ff98391fd2818ca5962946
yayamayaya <semenov.aleksandr@phystech.edu>
2023-11-24 00:59:28 +0300

    Добавил чтение регистров

--- Assembler/assembler.c
+++ Assembler/assembler.c
@@ -5,6 +5,7 @@
 #include <assert.h>
 #include "C:\Users\megas\Clearn\ilab\Onegin\fileSize.h"
 #include "C:\Users\megas\Clearn\ilab\Onegin\StringNumber.h"
+#include "asm.h"
 
 
 int main()
@@ -51,42 +52,86 @@ int main()
     {     
         if (strncmp(strCode[pos], "push ", 5) == 0)
         {
-            fprintf(bytecode, "%d ", 0);
-            int temp = 0;
+            int temp = 0;            
+            if(sscanf(strCode[pos] + 5, "%d", &temp) == 0)
+            {
+                fprintf(bytecode, "%d ", rpush);
+                sscanf(strCode[pos] + 5, "%c", &temp);
+                switch (temp)
+                {
+                case 97:
+                    fprintf(bytecode, "%d ", 1);
+                    break;
+                case 98:
+                    fprintf(bytecode, "%d ", 2);
+                    break;
+                case 99:
+                    fprintf(bytecode, "%d ", 3);
+                    break;
+                case 100:
+                    fprintf(bytecode, "%d ", 4);
+                    break;            
+                default:
+                    printf("Compilation error: incompatable register name");
+                    return 1;
+                    break;
+                }
+                continue;
+            }
+            fprintf(bytecode, "%d ", PUSH);
+
             sscanf(strCode[pos] + 5, "%d", &temp);
             fprintf(bytecode, "%d ", temp);
         }
-        else if (strcmp(strCode[pos], "plus\0") == 0)
-            fprintf(bytecode, "%d ", 101);
         else if (strcmp(strCode[pos], "add\0") == 0)
-            fprintf(bytecode, "%d ", 11);
+            fprintf(bytecode, "%d ", ADD);
         else if (strcmp(strCode[pos], "sub\0") == 0)
-            fprintf(bytecode, "%d ", 12);
+            fprintf(bytecode, "%d ", SUB);
         else if (strcmp(strCode[pos], "mult\0") == 0)
-            fprintf(bytecode, "%d ", 13);
+            fprintf(bytecode, "%d ", MULT);
         else if (strcmp(strCode[pos], "div\0") == 0)
-            fprintf(bytecode, "%d ", 14);
+            fprintf(bytecode, "%d ", DIV);
         else if (strcmp(strCode[pos], "out\0") == 0)
-            fprintf(bytecode, "%d ", 21);
-        else if (strcmp(strCode[pos], "halt\0") == 0)
+            fprintf(bytecode, "%d ", OUT);
+        else if (strcmp(strCode[pos], "in\0") == 0)
+            fprintf(bytecode, "%d ", IN);
+        else if (strncmp(strCode[pos], "pop ", 4) == 0)
         {
-            fprintf(bytecode, "%d", 210);
+
+            int temp = 0;
+            fprintf(bytecode, "%d ", POP);
+            sscanf(strCode[pos] + 4, "%c", &temp);
+            switch (temp)
+            {
+            case 97:
+                fprintf(bytecode, "%d ", 1);
+                break;
+            case 98:
+                fprintf(bytecode, "%d ", 2);
+                break;
+            case 99:
+                fprintf(bytecode, "%d ", 3);
+                break;
+            case 100:
+                fprintf(bytecode, "%d ", 4);
+                break;            
+            default:
+                printf("Compilation error: incompatable register name");
+                return 1;
+                break;
+            }
+        }
+        else if (strcmp(strCode[pos], "halt\0") == 0)           //Добавить условие необходимости halt
+        {
+            fprintf(bytecode, "%d", HALT);
             printf(">>> Compilation successful\n");
+            fclose(bytecode);
             return 0;
         }
         else
         {
-            printf("\n\n>>>Compilation error. Command not found.");
+            printf("\n\n>>>Compilation error.");
             return 1;
         }
     }
 }
-
-
-//0 - push
-//11 - add
-//12 - sub
-//13 - mult
-//14 - div
-//21 - out
-//210 - halt
