459baa14d6e342b1a2734801e6f400498807778f
yayamayaya <semenov.aleksandr@phystech.edu>
2023-11-24 01:02:10 +0300

    Добавил массив char'ов Buff, процессор теперь - - структура, добавил регистры, команду in.

--- Processor/processor.c
+++ Processor/processor.c
@@ -1,63 +1,111 @@
 #include <stdio.h>
-#include "C:\Users\megas\Clearn\ilab\Stack\stack.h"
+#include <stdlib.h>
+#include "..\Stack\stack.h"
+#include "..\Assembler\asm.h"
+#include "..\Onegin\fileSize.h"
+
+struct Processor 
+{
+    stack Stack;
+    int Register[4];
+};
 
 int main()
 {
-    FILE * bytecode = fopen("../Assembler/bytecode.txt", "r");       //Открываем файл
+    FILE * bytecode = fopen("../Assembler/bytecode.txt", "r");      
 
-    stack memory = {0};
-    stackCtor(&memory, 10);
+    int size = 0;                                          
 
-    for (int num = 0, temp1 = 0, temp2 = 0;; fscanf(bytecode, "%d", &num))
+    fileSize(bytecode, &size);
+    char *Buff = (char *)calloc(size + 1, sizeof(char));      
+
+    rewind(bytecode);                               
+    if(fread(Buff, sizeof(char), size, bytecode) < size)      
     {
-        getc(bytecode);
+        printf(">>>text reading error");
+        return 1;
+    }
+    fclose(bytecode); 
+
+    struct Processor processor = {0};
+    stackCtor(&processor.Stack, 10);
+
+    for (int num = 0, temp1 = 0, temp2 = 0, pos = 0, ch = 0;;)
+    {
+        sscanf(Buff + pos, "%d %n", &num, &ch);
+        pos += ch;
+
         switch (num)
         {
-        case 0:
-            fscanf(bytecode, "%d", &num);
-            stackPush(&memory, num);
+        case PUSH:
+            sscanf(Buff + pos, "%d %n", &num, &ch);
+            pos += ch;
+
+            stackPush(&processor.Stack, num);
             
             break;
-        case 11:
-            if(stackPop(&memory, &temp1))
-                return 1;
-            if(stackPop(&memory, &temp2))
-                return 1;
-            stackPush(&memory, temp2 + temp1);
+        case rpush:
+            sscanf(Buff + pos, "%d %n", &temp1, &ch);
+            pos += ch;
+
+            stackPush(&processor.Stack, processor.Register[temp1 - 1]);
+
             break;
-        case 12:
-            if(stackPop(&memory, &temp1))
-                return 1;
-            if(stackPop(&memory, &temp2))
-                return 1;
-            stackPush(&memory, temp2 - temp1);
+        case POP:
+            sscanf(Buff + pos, "%d %n", &temp1, &ch);
+            pos += ch;
+
+            stackPop(&processor.Stack, processor.Register + temp1 - 1);
+            
             break;
-        case 13:
-            if(stackPop(&memory, &temp1))
+        case ADD:
+            if(stackPop(&processor.Stack, &temp1))
                 return 1;
-            if(stackPop(&memory, &temp2))
+            if(stackPop(&processor.Stack, &temp2))
                 return 1;
-            stackPush(&memory, temp2 * temp1);
+            stackPush(&processor.Stack, temp2 + temp1);
             break;
-        case 14:
-            if(stackPop(&memory, &temp1))
+        case SUB:
+            if(stackPop(&processor.Stack, &temp1))
                 return 1;
-            if(stackPop(&memory, &temp2))
+            if(stackPop(&processor.Stack, &temp2))
                 return 1;
-            stackPush(&memory, temp2 / temp1);
+            stackPush(&processor.Stack, temp2 - temp1);
             break;
-        case 21:
+        case MULT:
+            if(stackPop(&processor.Stack, &temp1))
+                return 1;
+            if(stackPop(&processor.Stack, &temp2))
+                return 1;
+            stackPush(&processor.Stack, temp2 * temp1);
+            break;
+        case DIV:
+            if(stackPop(&processor.Stack, &temp1))
+                return 1;
+            if(stackPop(&processor.Stack, &temp2))
+                return 1;
+            stackPush(&processor.Stack, temp2 / temp1);
+            break;
+        case IN:
+            printf("Enter the number:\n");
+            scanf("%d", &temp1);
+
+            stackPush(&processor.Stack, temp1);
+            break;
+        case OUT:
             temp1 = 0;
-            while (stackPop(&memory, &temp1) != -1)
+            while (stackPop(&processor.Stack, &temp1) != -1)
                 printf("\n>> %d\n", temp1);    
             break;
-        case 210:
-            fclose(bytecode);          
-            stackDtor(&memory);
+        case HALT:
+            free(Buff);         
+            stackDtor(&processor.Stack);
             return 0;
+            break;
         default:
             printf("Error in computing");
             return 1;
+            break;
         }
     }
 }
