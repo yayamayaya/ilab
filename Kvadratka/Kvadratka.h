62e2c72a38dc90a1498156b52934aec99ce34bf8
yayamayaya <143596144+yayamayaya@users.noreply.github.com>
2023-11-26 13:47:34 +0300

    плохая квадратка

--- Kvadratka/Kvadratka.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-int aCondition(double a);
-int bCondition(double a, double b, double c, double *x1, double *x2);
-int bcCondition(double b, double c, double *x1, double *x2);
-int cCondition(double a, double b, double c, double *x1, double *x2);
-double Discriminant(double a, double b, double c);
-int kvUr(double a, double b, double c, double *x1, double *x2);
