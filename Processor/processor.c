c7d1ce393a882adbba163cd9222ef4da481ad969
yayamayaya <semenov.aleksandr@phystech.edu>
2023-12-04 23:01:14 +0300

    процессор обновлён для работы с байт кодом

--- Processor/processor.c
+++ Processor/processor.c
@@ -4,15 +4,28 @@
 #include "..\Assembler\asm.h"
 #include "..\Onegin\fileSize.h"
 
+#ifdef DEBUG
+    #define PRINT(arg) printf(arg);
+#else
+    #define PRINT(arg)
+#endif
+
 struct Processor 
 {
     stack Stack;
     int Register[4];
 };
 
+int arithmetics(stack *stackName, char action);
+
 int main()
 {
-    FILE * bytecode = fopen("../Assembler/bytecode.txt", "r");      
+    FILE * bytecode = fopen("../Assembler/bytecode.txt", "rb");
+    if(bytecode == NULL)
+    {
+        printf("Can't open the file.");
+        return 1;
+    }      
 
     int size = 0;                                          
 
@@ -30,72 +43,44 @@ int main()
     struct Processor processor = {0};
     stackCtor(&processor.Stack, 10);
 
-    for (int num = 0, temp1 = 0, temp2 = 0, pos = 0, ch = 0;;)
-    {
-        sscanf(Buff + pos, "%d %n", &num, &ch);
-        pos += ch;
 
-        switch (num)
+    for (int num = 0, pos = 0, temp = 0 ;; pos++)
+    {
+        switch (Buff[pos])
         {
         case PUSH:
-            sscanf(Buff + pos, "%d %n", &num, &ch);
-            pos += ch;
-
-            stackPush(&processor.Stack, num);
-            
+            ++pos;
+            stackPush(&processor.Stack, Buff[pos]);
             break;
         case rpush:
-            sscanf(Buff + pos, "%d %n", &temp1, &ch);
-            pos += ch;
-
-            stackPush(&processor.Stack, processor.Register[temp1 - 1]);
-
+            ++pos;
+            stackPush(&processor.Stack, processor.Register[Buff[pos] - 1]);
             break;
         case POP:
-            sscanf(Buff + pos, "%d %n", &temp1, &ch);
-            pos += ch;
-
-            stackPop(&processor.Stack, processor.Register + temp1 - 1);
-            
+            ++pos;
+            stackPop(&processor.Stack, processor.Register + Buff[pos] - 1);
             break;
         case ADD:
-            if(stackPop(&processor.Stack, &temp1))
-                return 1;
-            if(stackPop(&processor.Stack, &temp2))
-                return 1;
-            stackPush(&processor.Stack, temp2 + temp1);
+            arithmetics(&processor.Stack, '+');
             break;
         case SUB:
-            if(stackPop(&processor.Stack, &temp1))
-                return 1;
-            if(stackPop(&processor.Stack, &temp2))
-                return 1;
-            stackPush(&processor.Stack, temp2 - temp1);
+            arithmetics(&processor.Stack, '-');
             break;
         case MULT:
-            if(stackPop(&processor.Stack, &temp1))
-                return 1;
-            if(stackPop(&processor.Stack, &temp2))
-                return 1;
-            stackPush(&processor.Stack, temp2 * temp1);
+            arithmetics(&processor.Stack, '*');
             break;
         case DIV:
-            if(stackPop(&processor.Stack, &temp1))
-                return 1;
-            if(stackPop(&processor.Stack, &temp2))
-                return 1;
-            stackPush(&processor.Stack, temp2 / temp1);
+            arithmetics(&processor.Stack, '/');
             break;
         case IN:
             printf("Enter the number:\n");
-            scanf("%d", &temp1);
+            scanf("%d", &temp);
 
-            stackPush(&processor.Stack, temp1);
+            stackPush(&processor.Stack, temp);
             break;
         case OUT:
-            temp1 = 0;
-            while (stackPop(&processor.Stack, &temp1) != -1)
-                printf("\n>> %d\n", temp1);    
+            while (stackPop(&processor.Stack, temp) != -1)    //пустой поп
+                printf("\n--> %d\n", temp);    
             break;
         case HALT:
             free(Buff);         
@@ -107,5 +92,36 @@ int main()
             return 1;
             break;
         }
+    }
+}
+
+int arithmetics(stack *stackName, char action)
+{
+    int temp1 = 0;
+    int temp2 = 0;
+
+    if(stackPop(stackName, &temp1))
+        return 1;
+    if(stackPop(stackName, &temp2))
+        return 1;
+       
+    switch (action)
+    {
+    case '+':
+        stackPush(stackName, temp1 + temp2);
+        break;
+    case '-':
+        stackPush(stackName, temp1 - temp2);
+        break;
+    case '*':
+        stackPush(stackName, temp1 * temp2);
+        break;
+    case '/':
+        stackPush(stackName, temp1 / temp2);
+        break;
+    default:
+        //Ошибка действия
+        return 0;
+        break;
     }
 }
