eb07a71714741ce6e66a71550dc8e99d8f316743
yayamayaya <semenov.aleksandr@phystech.edu>
2023-12-05 23:19:43 +0300

    Переработан стек: э добавлен нормальный дебаггинг, стекПринт пишет всю информацию о стеке добавлены ассерты, добавлен пустой поп

--- Stack/stack.c
+++ Stack/stack.c
@@ -5,10 +5,21 @@
 
 #define DEBUG
 
-#define STK_CTED  0
-#define STK_DTED "\n>> Stack has been destructed.\n" 0
-#define MEM_ALC_ERR "\n>>>Memory allocation error" 0
-#define MEM_RLC_ERR "\n>>>Memory reallocation error" 
+#ifdef DEBUG
+    #define PRINT(arg) printf(arg);
+#else
+    #define PRINT(arg)
+#endif
+
+#define STK_CTED  "\n>> Stack has been constructed with the capacity of: %d", stk-> capacity 
+#define STK_DTED "\n>> Stack has been destructed" 
+#define MEM_ALC_ERR "\n\n>>>Memory allocation error" 
+#define MEM_RLC_ERR "\n\n>>>Memory reallocation error" 
+#define STK_PUSH "\n>> %d was pushed on position %d", *(stk-> data + stk-> size - 1), stk-> size
+#define STK_POP "\n>> %d was popped from the stack", temp
+#define STK_EMPTY "\n>> Stack is empty"
+#define CAPACITY_UP "\n>> Capacity has been doubled"
+#define CAPACITY_DOWN "\n>> Capacity has been halfed"
 
 enum stk_realloc
 {
@@ -16,110 +27,100 @@ enum stk_realloc
     UP = 1
 };
 
-//Написать typedef для быстрой смены типа
-//Проверять, чтобы не подсунули 0 указатель в функцию
-
 int stackCtor(stack *stk, int capacity)
 {
-    stk-> data = (int *)calloc(capacity, sizeof(int));      //Проверка на каллок
+    assert(stk != NULL && capacity > 0);
+    stk-> data = (sDT *)calloc(capacity, sizeof(sDT));
     if(stk-> data == NULL)
     {
-        #ifdef DEBUG
-            printf("\n>>>Memory allocation error");
-        #endif
-        return 1; //Убрать exit'ы
+        PRINT(MEM_ALC_ERR);
+        return 1;
     }
 
     stk-> size = 0;
     stk-> capacity = capacity;
 
-    #ifdef DEBUG
-        printf("\n>>Stack has been constructed with the capacity of %d\n", stk-> capacity);
-    #endif
+    PRINT(STK_CTED)
     
     return 0;
 }
 
 void stackDtor(stack *stk)
 {
+    assert(stk != NULL);
     stk-> capacity = 0;
     stk-> size = 0;
 
     free(stk-> data);
     stk-> data = NULL;
 
-    #ifdef DEBUG
-        printf("\n>> Stack has been destructed.\n");
-    #endif
+    PRINT(STK_DTED);
 }
 
-void stackPush(stack *stk, int num)
+int stackPush(stack *stk, sDT num)
 {
+    assert(stk != NULL);
     if (stk-> size == stk-> capacity)
-        stk_realloc(stk, UP);
+        if(stk_realloc(stk, UP) != 0)
+            return 1;
     
-    *(stk-> data + stk-> size) = num;
+    stk-> data[stk-> size] = num;
     stk-> size++;
 
-    #ifdef DEBUG
-        printf(">> %d was pushed on position %d.\n", *(stk-> data + stk-> size - 1), stk-> size);
-    #endif
+    PRINT(STK_PUSH)
+    return 0;
 }
 
-int stackPop(stack *stk, int *num) //Указатель на значение.
+int stackPop(stack *stk, sDT *num)
 {
+    assert(stk != NULL);
     if (stk-> size == 0)
     {
-        #ifdef DEBUG
-            printf(">> Stack is empty \n");
-        #endif
-
+        PRINT(STK_EMPTY)
         return -1;
     }
     
     --stk-> size;   
-    int temp = *(stk-> data + stk-> size);  //Сделать пустой поп, если num == NULL
-    *(stk-> data + stk-> size) = 0;
+    sDT temp = stk-> data[stk-> size];
+    stk-> data[stk-> size] = 0; //Инициализация ядом???
  
-
-    #ifdef DEBUG
-    printf(">> Last number was popped. Current free position is %d\n", stk->size); //Инициализация ядом.
-    #endif
+    PRINT(STK_POP)
 
     if(num != NULL)
-    {
         *num = temp;
-    }
+
     return 0;
 }
 
 void stackPrint(stack *stk)
 {
-    printf(">> Current data in stack:\n");
+    assert(stk != NULL);
+    printf("\n>> Stack info:\n");
+    printf("\n>> Size: %d", stk-> size);
+    printf("\n>> Capacity: %d", stk-> capacity);
+    printf("\n>> Current data in stack:");    
     for (int i = 0; i < stk-> size; i++)
-    {
-        printf("%d) %d\n", i+1, *(stk-> data + i)); //Написать всю информацию о стеке
-    }
+        printf("%d) %d\n", i + 1, *(stk-> data + i));
 }
 
 int stk_realloc(stack *stk, int num)
 {
+    assert(stk != NULL);
     if(num == UP)
-        stk ->capacity *= 2;
-    else if(num == DOWN)
-        stk ->capacity /= 2;          
-    if(realloc(stk-> data, stk-> capacity * sizeof(int)) == NULL)        
     {
-        #ifdef DEBUG
-            printf("\n>>>Memory reallocation error");
-        #endif
-
+        stk ->capacity *= 2;
+        PRINT(CAPACITY_UP)
+    }
+    else if(num == DOWN)
+    {
+        stk ->capacity /= 2;
+        PRINT(CAPACITY_DOWN)           
+    }
+    if(realloc(stk-> data, stk-> capacity * sizeof(sDT)) == NULL)        
+    {
+        PRINT(MEM_RLC_ERR)
         return 1;
     }
 
-    #ifdef DEBUG
-        printf("\n>> Not enough capacity. Capacity has been doubled. Current capacity - %d\n", stk-> capacity);
-    #endif
+    return 0;
 }
-
-//Stack dump
--- Stack/stack.h
+++ Stack/stack.h
@@ -6,10 +6,13 @@ struct stack
     int size;
     int capacity;
 };
+
+typedef int sDT;
 typedef struct stack stack;
+
 int stackCtor(stack *stk, int capacity);
 void stackDtor(stack *stk);
-void stackPush(stack *stk, int num);
-int stackPop(stack *stk, int *num);
+int stackPush(stack *stk, sDT num);
+int stackPop(stack *stk, sDT *num);
 void stackPrint(stack *stk);
 int stk_realloc(stack *stk, int num);
