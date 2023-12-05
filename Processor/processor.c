3713ac13ebb35f94cc01fcfe037f62d022a78674
yayamayaya <semenov.aleksandr@phystech.edu>
2023-12-05 23:15:29 +0300

    Добавлены циклы и условия, соответствующая им функция

--- Processor/processor.c
+++ Processor/processor.c
@@ -2,13 +2,7 @@
 #include <stdlib.h>
 #include "..\Stack\stack.h"
 #include "..\Assembler\asm.h"
-#include "..\Onegin\fileSize.h"
-
-#ifdef DEBUG
-    #define PRINT(arg) printf(arg);
-#else
-    #define PRINT(arg)
-#endif
+#include  "..\Onegin\fileReader.h"
 
 struct Processor 
 {
@@ -17,70 +11,86 @@ struct Processor
 };
 
 int arithmetics(stack *stackName, char action);
+int condJMP(stack *stackName, char *arr, int *ip, char condition);
+int jmp(char* arr, int *ip);
 
 int main()
 {
-    FILE * bytecode = fopen("../Assembler/bytecode.txt", "rb");
-    if(bytecode == NULL)
-    {
-        printf("Can't open the file.");
-        return 1;
-    }      
+    char* Buff = 0;
 
-    int size = 0;                                          
-
-    fileSize(bytecode, &size);
-    char *Buff = (char *)calloc(size + 1, sizeof(char));      
-
-    rewind(bytecode);                               
-    if(fread(Buff, sizeof(char), size, bytecode) < size)      
-    {
-        printf(">>>text reading error");
-        return 1;
-    }
-    fclose(bytecode); 
+    fileRead("../Assembler/bytecode.txt", &Buff, NULL, NULL, NULL, BUFF_ONLY);
 
     struct Processor processor = {0};
     stackCtor(&processor.Stack, 10);
 
 
-    for (int num = 0, pos = 0, temp = 0 ;; pos++)
+    for (int num = 0, ip = 0, temp = 0 ;;)
     {
-        switch (Buff[pos])
+        switch (Buff[ip])
         {
         case PUSH:
-            ++pos;
-            stackPush(&processor.Stack, Buff[pos]);
+            ++ip;
+            stackPush(&processor.Stack, Buff[ip]);
+            ip++;
             break;
         case rpush:
-            ++pos;
-            stackPush(&processor.Stack, processor.Register[Buff[pos] - 1]);
+            ++ip;
+            stackPush(&processor.Stack, processor.Register[Buff[ip] - 1]);
+            ip++;
             break;
         case POP:
-            ++pos;
-            stackPop(&processor.Stack, processor.Register + Buff[pos] - 1);
+            ++ip;
+            stackPop(&processor.Stack, processor.Register + Buff[ip] - 1);
+            ip++;
             break;
         case ADD:
             arithmetics(&processor.Stack, '+');
+            ip++;
             break;
         case SUB:
             arithmetics(&processor.Stack, '-');
+            ip++;
             break;
         case MULT:
             arithmetics(&processor.Stack, '*');
+            ip++;
             break;
         case DIV:
             arithmetics(&processor.Stack, '/');
+            ip++;
             break;
         case IN:
             printf("Enter the number:\n");
             scanf("%d", &temp);
 
             stackPush(&processor.Stack, temp);
+            ip++;
+            break;
+        case JMP:
+            jmp(Buff, &ip);
+            break;
+        case JB:
+            condJMP(&processor.Stack, Buff, &ip, '<');
+            break;
+        case JBE:
+            condJMP(&processor.Stack, Buff, &ip, 'l');
+            break;
+        case JA:
+            condJMP(&processor.Stack, Buff, &ip, '>');
+            break;
+        case JAE:
+            condJMP(&processor.Stack, Buff, &ip, 'm');
+            break;
+        case JE:
+            condJMP(&processor.Stack, Buff, &ip, '=');
+            break;
+        case JNE:
+            condJMP(&processor.Stack, Buff, &ip, '!');
             break;
         case OUT:
-            while (stackPop(&processor.Stack, temp) != -1)    //пустой поп
+            while (stackPop(&processor.Stack, &temp) != -1)    //пустой поп
                 printf("\n--> %d\n", temp);    
+            ip++;
             break;
         case HALT:
             free(Buff);         
@@ -123,5 +133,67 @@ int arithmetics(stack *stackName, char action)
         //Ошибка действия
         return 0;
         break;
+    }
+}
+
+int jmp(char* arr, int *ip)
+{
+    *ip = arr[*ip + 1];
+}
+
+int condJMP(stack *stackName, char *arr, int *ip, char condition)
+{
+    int temp1 = 0;
+    int temp2 = 0;
+
+    if(stackPop(stackName, &temp1))
+        return 1;
+    if(stackPop(stackName, &temp2))
+        return 1;
+
+    stackPush(stackName, temp2);
+    stackPush(stackName, temp1);
+
+    switch(condition)
+    {
+        case '<':
+            if(temp2 < temp1)   //меньше
+                jmp(arr, ip);
+            else
+                *ip += 2;
+            break;
+        case '>':
+            if(temp2 > temp1)   //больше
+                jmp(arr, ip);
+            else
+                *ip += 2;
+            break;
+        case '=':
+            if(temp2 == temp1)  //равно
+                jmp(arr, ip);
+            else
+                *ip += 2;
+            break;
+        case '!':
+            if(temp2 != temp1)  //не равно
+                jmp(arr, ip);
+            else
+                *ip += 2;
+            break;
+        case 'm':
+            if(temp2 >= temp1)  //больше или равно
+                jmp(arr, ip);
+            else
+                *ip += 2;
+            break;
+        case 'l':
+            if(temp2 <= temp1)  //меньше или равно
+                jmp(arr, ip);
+            else
+                *ip += 2;
+            break;
+        default:
+            printf("\n>>ERROR\n\n");
+            break;    
     }
 }
