9e1944c51c66b0694c6c301ff277e0b221364d42
yaya mayaya <semenov.aleksandr@phystech.edu>
2023-12-19 13:47:08 +0300

    неболшие изменения

--- Stack/stack.h
+++ Stack/stack.h
@@ -5,14 +5,39 @@ struct stack
     int* data;
     int size;
     int capacity;
+    long long int *firstCanaryPtr;
+    long long int *secondCanaryPtr;
 };
 
-typedef int sDT;
+typedef int dataType;
 typedef struct stack stack;
 
-int stackCtor(stack *stk, int capacity);
+enum debugging
+{
+    STK_CTED = 0,
+    CAPACITY_ZERO = 1,
+    MEM_ALC_ERR = 2,
+    MEM_RLC_ERR = 3,
+    STK_PUSH = 4,
+    STK_POP = 5,
+    STK_EMPTY = 6,
+    CAPACITY_UP = 7,
+    CAPACITY_DOWN = 8,
+    STK_DTED = 9,
+    PSN_ERR = 10,
+    CANARY_ERR = 11,
+    STK_KILL = -1
+};
+
+enum stk_realloc
+{
+    DOWN = 0, 
+    UP = 1
+};
+
+int stackCtor(stack *stk, const int capacity);
 void stackDtor(stack *stk);
-int stackPush(stack *stk, sDT num);
-int stackPop(stack *stk, sDT *num);
-void stackPrint(stack *stk);
-int stk_realloc(stack *stk, int num);
+int stackPush(stack *stk, const dataType num);
+int stackPop(stack *stk, dataType *num);
+void stackPrint(const stack *pstk);
+int stk_realloc(stack *stk, const int num);
